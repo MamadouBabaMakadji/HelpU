e023bb1d489ef16652e72725b15a4869f0c75e8d
MamadouBabaMakadji <mamadoubaba1@hotmail.fr>
2017-02-03 12:33:04 +0100

    Validate (xml) become helper

---
+++ app/src/main/java/fr/miage/paris10/projetm1/helpu/BecomeHelperActivity.java
@@ -0,0 +1,25 @@
+package fr.miage.paris10.projetm1.helpu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BecomeHelperActivity extends AppCompatActivity {
+
+    private Button btn_valider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_become_helper);
+        btn_valider = (Button) findViewById(R.id.button_valider);
+        btn_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BecomeHelperActivity.this,ValidationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
