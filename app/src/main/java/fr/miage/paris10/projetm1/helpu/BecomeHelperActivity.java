de11d3de18da6fde46325e0d5aba8e4048247782
Joan Medjid <j.medjid@orange.fr>
2017-02-28 12:55:34 +0100

    Add reset password option

---
+++ app/src/main/java/fr/miage/paris10/projetm1/helpu/BecomeHelperActivity.java
@@ -0,0 +1,52 @@
+package fr.miage.paris10.projetm1.helpu;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BecomeHelperActivity extends AppCompatActivity{
+
+  private Button btn_valider;
+  private EditText ecText;
+  private FirebaseAuth mFirebaseAuth;
+  private DatabaseReference mDatabaseReference;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_become_helper);
+
+    // Initialize FirebaseAuth
+    mFirebaseAuth = FirebaseAuth.getInstance();
+    mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+    btn_valider = (Button) findViewById(R.id.button_valider);
+    ecText = (EditText) findViewById(R.id.input_ec);
+
+
+    btn_valider.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(BecomeHelperActivity.this,ValidationActivity.class);
+        startActivity(intent);
+      }
+    });
+
+    //String ec = ecText.getText().toString();
+
+
+
+
+
+
+
+  }
+}
--- app/src/main/java/fr/miage/paris10/projetm1/helpu/LoginActivity.java
+++ app/src/main/java/fr/miage/paris10/projetm1/helpu/LoginActivity.java
@@ -4,8 +4,14 @@ package fr.miage.paris10.projetm1.helpu;
  * Created by david on 24/01/2017.
  */
 
+
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -16,22 +22,38 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.ButterKnife;
 import butterknife.Bind;
+
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
-
+    private FirebaseAuth mFirebaseAuth;
     @Bind(R.id.input_email) EditText _emailText;
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.btn_login) Button _loginButton;
     @Bind(R.id.link_signup) TextView _signupLink;
+    @Bind(R.id.link_reset_password) TextView _resetPasswordLink;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
         ButterKnife.bind(this);
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
@@ -42,8 +64,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        _signupLink.setOnClickListener(new View.OnClickListener() {
+        _resetPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });
 
+        _signupLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Start the Signup activity
@@ -58,33 +86,69 @@ public class LoginActivity extends AppCompatActivity {
     public void login() {
         Log.d(TAG, "Login");
 
-        if (!validate()) {
-            onLoginFailed();
-            return;
+        if(!isOnline())
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            builder.setMessage(R.string.internet_error_message)
+                    .setTitle(R.string.signup_error_title);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }else{
+
+            if (!validate()) {
+                onLoginFailed();
+                return;
+            }
+
+            //_loginButton.setEnabled(false);
+
+            final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                    R.style.AppTheme_Dark_Dialog);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Authenticating...");
+            progressDialog.show();
+
+            // TODO: Implement your own authentication logic here.
+
+
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            String email = _emailText.getText().toString();
+                            String password = _passwordText.getText().toString();
+                            mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+
+                                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                                if( user.isEmailVerified()){
+                                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                    startActivity(intent);
+                                                    onLoginSuccess();
+                                                }
+                                                else{
+                                                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                                    builder.setMessage(R.string.verify_email_error_message)
+                                                            .setTitle(R.string.signup_error_title);
+                                                    AlertDialog dialog = builder.create();
+                                                    dialog.show();
+                                                }
+                                            } else {
+                                                onLoginFailed();
+                                            }
+                                        }
+                                    });
+
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
+
         }
 
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
     }
 
 
@@ -107,14 +171,14 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginSuccess() {
-        _loginButton.setEnabled(true);
+       // _loginButton.setEnabled(true);
         finish();
     }
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
-        _loginButton.setEnabled(true);
+        //_loginButton.setEnabled(true);
     }
 
     public boolean validate() {
@@ -123,10 +187,18 @@ public class LoginActivity extends AppCompatActivity {
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
+        Pattern p = Pattern.compile(".+@u-paris10.fr+");
+        Matcher m = p.matcher(email);
+
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            _emailText.setError("Enter a valid email address");
             valid = false;
-        } else {
+        }
+        else if (!m.matches()){
+            _emailText.setError("Domain name must be u-paris10.fr");
+            valid = false;
+        }
+        else {
             _emailText.setError(null);
         }
 
@@ -139,4 +211,16 @@ public class LoginActivity extends AppCompatActivity {
 
         return valid;
     }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) { // connected to the internet
+            return true;
+        }
+        return false;
+
+    }
+
 }
