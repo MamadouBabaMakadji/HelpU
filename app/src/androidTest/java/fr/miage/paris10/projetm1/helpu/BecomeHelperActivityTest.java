cffc441cc847112d22a83642bf2f27435d3f5960
david <david.meimoun@hotmail.fr>
2017-03-15 02:48:09 +0100

    fixed database

--- app/src/androidTest/java/fr/miage/paris10/projetm1/helpu/BecomeHelperActivityTest.java
+++ app/src/androidTest/java/fr/miage/paris10/projetm1/helpu/BecomeHelperActivityTest.java
@@ -7,13 +7,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+
 
 /**
  * Created by david on 3/14/17.
--- app/src/androidTest/java/fr/miage/paris10/projetm1/helpu/MainActivityTest.java
+++ app/src/androidTest/java/fr/miage/paris10/projetm1/helpu/MainActivityTest.java
@@ -1,18 +1,11 @@
 package fr.miage.paris10.projetm1.helpu;
 
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Created by david on 3/14/17.
